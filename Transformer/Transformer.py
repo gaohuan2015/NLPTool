4a94884747cd9939c1dc273f60253d09fd7a684f
unknown <53857658@qq.com>
2019-04-16 21:22:59 +0800

    fix some bug

--- Transformer/Transformer.py
+++ Transformer/Transformer.py
@@ -3,12 +3,15 @@ import torch.nn as nn
 
 
 class Transformer(nn.Module):
-    def __init__(self, embedding_layer, encoder_layer, decoder_layer):
+    def __init__(self, embedding_layer1, embedding_layer2, encoder_layer, decoder_layer):
         self.encoder = encoder_layer
-        self.embedding = embedding_layer
+        self.scr_embedding = embedding_layer1
+        self.tg_embedding = embedding_layer2
         self.decoder = decoder_layer
 
-    def forward(self, x):
-        x_embed = self.embedding(x)
-        x_encoder = self.encoder(x)
-        x_encoder = self.decoder(x, e)
+    def forward(self, s, t):
+        x_embed = self.scr_embedding(s)
+        x_encoder = self.encoder(x_embed)
+        t_embed = self.tg_embedding(t)
+        target = self.decoder(t_embed, x_encoder)
+        return target
