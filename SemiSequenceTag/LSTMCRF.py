179bb3f927bfdb9b9be44bb0344ee6ddda8f6ce1
53857658@qq.com <53857658@qq.com>
2019-04-21 23:07:35 +0800

    edit format

--- SemiSequenceTag/LSTMCRF.py
+++ SemiSequenceTag/LSTMCRF.py
@@ -6,25 +6,32 @@ from Utilities import *
 
 
 class LSTMCRF(nn.Module):
-    def __init__(self, vocab_size, embedding_size, hidden_size, layer_number, tag_size):
+    def __init__(self, vocab_size, embedding_size, hidden_size, layer_number,
+                 tag_size):
         super(LSTMCRF, self).__init__()
         self.layer_num = layer_number
         self.hidden_dim = hidden_size
         self.tag_size = tag_size
         self.embedding_layer = nn.Embedding(vocab_size, embedding_size)
-        self.lstm = nn.LSTM(embedding_size, hidden_size//2,
-                            layer_number, batch_first=True, bidirectional=True)
+        self.lstm = nn.LSTM(embedding_size,
+                            hidden_size // 2,
+                            layer_number,
+                            batch_first=True,
+                            bidirectional=True)
         self.hid2tag = nn.Linear(hidden_size, tag_size)
 
     def init_weight(self, data):
-        return (torch.randn(self.layer_num*2, data.shape[0], self.hidden_dim // 2),
-                torch.randn(self.layer_num*2, data.shape[0], self.hidden_dim // 2))
+        return (torch.randn(self.layer_num * 2, data.shape[0],
+                            self.hidden_dim // 2),
+                torch.randn(self.layer_num * 2, data.shape[0],
+                            self.hidden_dim // 2))
 
     def lstm_feature(self, x, length):
         h = self.init_weight(x)
         embed = self.embedding_layer(x)
-        packed_embedding = rnn_utils.pack_padded_sequence(
-            embed, length, batch_first=True)
+        packed_embedding = rnn_utils.pack_padded_sequence(embed,
+                                                          length,
+                                                          batch_first=True)
         out, _ = self.lstm(packed_embedding, h)
         out, _ = rnn_utils.pad_packed_sequence(out, batch_first=True)
         tag_score = self.hid2tag(out)
@@ -41,8 +48,10 @@ if __name__ == "__main__":
     word_2_idx = {}
     tag_to_ix = {"b": 0, "i": 1, "o": 2, "<start>": 3, "<stop>": 4}
     # read data
-    training_data = ["the wall street journal reported today that apple corporation made money",
-                     "georgia tech is a university in georgia"]
+    training_data = [
+        "the wall street journal reported today that apple corporation made money",
+        "georgia tech is a university in georgia"
+    ]
     tag_data = ["B I I I O O O B I O O", "B I O O O O B"]
     sentences_to_id = []
     tag_to_id = []
@@ -51,8 +60,11 @@ if __name__ == "__main__":
         sentences_to_id.append(prepare_sequence(s, word_2_idx))
         tag_to_id.append(prepare_sequence(t, tag_to_ix))
     training = UnlabelData(sentences_to_id)
-    dataloader = data.DataLoader(
-        training, batch_size=2, shuffle=False, num_workers=4, collate_fn=padd_sentence_crf)
-    model = LSTMCRF(len(word_2_idx)+1, 100, 50, 2, len(tag_to_ix)+1)
+    dataloader = data.DataLoader(training,
+                                 batch_size=2,
+                                 shuffle=False,
+                                 num_workers=4,
+                                 collate_fn=padd_sentence_crf)
+    model = LSTMCRF(len(word_2_idx) + 1, 100, 50, 2, len(tag_to_ix) + 1)
     for s, l in dataloader:
         print(model(s, l))
