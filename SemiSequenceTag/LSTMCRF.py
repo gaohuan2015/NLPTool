69e514245f9970e707886d0ffae36f33fa045041
53857658@qq.com <53857658@qq.com>
2019-04-23 00:07:25 +0800

    fix bugs

--- SemiSequenceTag/LSTMCRF.py
+++ SemiSequenceTag/LSTMCRF.py
@@ -6,6 +6,7 @@ from Utilities import *
 from SequenceTag import *
 from tqdm import *
 import matplotlib.pyplot as plt
+import torch.autograd as autograd
 
 
 class LSTMCRF(nn.Module):
@@ -15,22 +16,24 @@ class LSTMCRF(nn.Module):
         self.layer_num = layer_number
         self.start_tag = '<start>'
         self.end_tag = '<stop>'
+        self.pad_tag = "<pad>"
         self.hidden_dim = hidden_size
         self.tag_to_idx = tag_to_idx
         self.tag_size = len(tag_to_idx)
         self.embedding_layer = nn.Embedding(vocab_size, embedding_size)
-        self.lstm = nn.LSTM(
-            embedding_size,
-            hidden_size // 2,
-            layer_number,
-            batch_first=True,
-            bidirectional=True)
+        self.lstm = nn.LSTM(embedding_size,
+                            hidden_size // 2,
+                            layer_number,
+                            batch_first=True,
+                            bidirectional=True)
         self.hid2tag = nn.Linear(hidden_size, self.tag_size)
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.transition = nn.Parameter(
             torch.randn(self.tag_size, self.tag_size)).to(device)
         self.transition[tag_to_idx[self.start_tag], :] = -10000
         self.transition[:, tag_to_idx[self.end_tag]] = -10000
+        self.transition[tag_to_idx[self.pad_tag], :] = 0
+        self.transition[:, tag_to_idx[self.pad_tag]] = 0
 
     def init_weight(self, data):
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -42,8 +45,9 @@ class LSTMCRF(nn.Module):
     def lstm_feature(self, x, length):
         h = self.init_weight(x)
         embed = self.embedding_layer(x)
-        packed_embedding = rnn_utils.pack_padded_sequence(
-            embed, length, batch_first=True)
+        packed_embedding = rnn_utils.pack_padded_sequence(embed,
+                                                          length,
+                                                          batch_first=True)
         out, _ = self.lstm(packed_embedding, h)
         out, _ = rnn_utils.pad_packed_sequence(out, batch_first=True)
         tag_score = self.hid2tag(out)
@@ -68,23 +72,24 @@ class LSTMCRF(nn.Module):
             score = score + self.transition[
                 tags[:, i + 1], tags[:, i]] + feat[:, tags[:, i + 1]][:, 0]
 
-        score = score + self.transition[tags[:, self.tag_to_idx[self.end_tag]],
-                                        tags[:, -1]]
+        score = score + self.transition[self.tag_to_idx[self.
+                                                        end_tag], tags[:, -1]]
         return score[0]
 
     def neg_log_likehood(self, sentence, tags, length):
         feature = self.lstm_feature(sentence, length)
         forward_score = self.forward_inference(feature)
         sentence_score = self.sentence_score(feature, tags)
-        return torch.sum(forward_score - sentence_score) / sentence.size(0)
+        return torch.mean(forward_score - sentence_score)
 
     def forward_inference(self, lstm_feature):
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         batch_size = lstm_feature.size(0)
-        init_alphas = torch.full((batch_size, self.tag_size),
-                                 -10000).to(device)
+        init_alphas = torch.full((batch_size, self.tag_size), -10000)
         init_alphas[:, self.tag_to_idx[self.start_tag]] = 0
-        forward_var = init_alphas
+        forward_var = autograd.Variable(init_alphas)
+        forward_var = forward_var.to(device)
+
         for feature in lstm_feature.permute(1, 0, 2):
             alphas = []
             for next_tag in range(self.tag_size):
@@ -98,8 +103,8 @@ class LSTMCRF(nn.Module):
             forward_var = torch.cat(alphas, 1).view(batch_size, -1)
         terminal_var = forward_var + self.transition[self.tag_to_idx[
             self.end_tag]]
-        alphas = self.log_exp_sum(terminal_var)
-        return alphas
+        forward_score = self.log_exp_sum(terminal_var)
+        return forward_score
 
     def forward(self, x, len):
         lstm = self.lstm_feature(x, len)
@@ -110,19 +115,18 @@ class LSTMCRF(nn.Module):
 if __name__ == "__main__":
     batchsize = 2
     word_2_idx = {}
-    tag_to_ix = {"b": 0, "i": 1, "o": 2, "<start>": 3, "<stop>": 4}
+    tag_to_ix = {"b": 0, "i": 1, "o": 2, "<start>": 3, "<stop>": 4, "<pad>": 5}
     # read data
     training_data = [
         "the wall street journal reported today that apple corporation made money",
         "georgia tech is a university in georgia",
         'Jean Pierre lives in New York',
-        #'The European Union is a political and economic union',
-        #'A French American actor won an oscar'
+        'The European Union is a political and economic union',
+        'A French American actor won an oscar'
     ]
     tag_data = [
-        "B I I I O O O B I O O",
-        "B I O O O O B",   "B I O O B I",
-        #"O B I O O O O O O", "O B I O O O O"
+        "B I I I O O O B I O O", "B I O O O O B", "B I O O B I",
+        "O B I O O O O O O", "O B I O O O O"
     ]
     sentences_to_id = []
     tag_to_id = []
@@ -131,15 +135,14 @@ if __name__ == "__main__":
         sentences_to_id.append(prepare_sequence(s, word_2_idx))
         tag_to_id.append(prepare_sequence(t, tag_to_ix))
     training = SequenceTag(sentences_to_id, tag_to_id)
-    dataloader = data.DataLoader(
-        training,
-        batch_size=batchsize,
-        shuffle=False,
-        num_workers=4,
-        collate_fn=padd_sentence_crf)
+    dataloader = data.DataLoader(training,
+                                 batch_size=batchsize,
+                                 shuffle=False,
+                                 num_workers=4,
+                                 collate_fn=padd_sentence_crf)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = LSTMCRF(len(word_2_idx) + 1, 100, 50, 2, tag_to_ix).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
     x = []
     y = []
     for epoch in tqdm(range(50)):
@@ -147,8 +150,8 @@ if __name__ == "__main__":
         for s, t, l in dataloader:
             model.zero_grad()
             l = torch.tensor(l, dtype=torch.long)
-            loss = model.neg_log_likehood(
-                s.to(device), t.to(device), l.to(device))
+            loss = model.neg_log_likehood(s.to(device), t.to(device),
+                                          l.to(device))
             loss.backward(retain_graph=True)
             optimizer.step()
             total_loss = total_loss + loss
--- SemiSequenceTag/Utilities.py
+++ SemiSequenceTag/Utilities.py
@@ -10,8 +10,9 @@ def padd_sentence(sentences):
     for i in range(len(sentences_length)):
         if sentences_length[i] == max_length:
             sentences_length[i] = sentences_length[i] - 1
-    sentences = rnn_utils.pad_sequence(
-        sentences, batch_first=True, padding_value=0)
+    sentences = rnn_utils.pad_sequence(sentences,
+                                       batch_first=True,
+                                       padding_value=0)
     return sentences, sentences_length
 
 
@@ -23,9 +24,10 @@ def padd_sentence_crf(SequenceTag):
         tags.append(t)
     sentences.sort(key=lambda x: len(x), reverse=True)
     sentences_length = [len(x) for x in sentences]
-    sentences = rnn_utils.pad_sequence(
-        sentences, batch_first=True, padding_value=0)
-    tags = rnn_utils.pad_sequence(tags, batch_first=True, padding_value=4)
+    sentences = rnn_utils.pad_sequence(sentences,
+                                       batch_first=True,
+                                       padding_value=0)
+    tags = rnn_utils.pad_sequence(tags, batch_first=True, padding_value=5)
     return sentences, tags, sentences_length
 
 
